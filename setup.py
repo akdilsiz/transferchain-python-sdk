fd3ced56f3eae633079827cd13791e613ecc37c8
ayhan.ozemre <ayhan.ozemre@gmail.com>
2023-11-03 20:19:01 +0300

    readthedocs conf created

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name="transferchain-python-sdk",
-    version="0.1.0",
+    version="0.1.1",
     author="TransferChain",
     author_email="info@transferchain.io",
     description="TransferChain Python SDK",
