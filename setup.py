d8fb9dcc976a770312035b2d4bdb2e3c9b724ff8
ayhan.ozemre <ayhan.ozemre@gmail.com>
2023-11-03 20:49:41 +0300

    version updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name="transferchain-python-sdk",
-    version="0.1.3",
+    version="0.1.2",
     author="TransferChain",
     author_email="info@transferchain.io",
     description="TransferChain Python SDK",
